1268d914c8821b5743b552670beb554cbd0a3316
Freddie <freddiejscott@outlook.com>
2023-04-29 09:35:00 +0100

    Add project files.

---
+++ BoxyProgrammingLanguage/BoxyProgrammingLanguage.cpp
@@ -0,0 +1,12 @@
+﻿// BoxyProgrammingLanguage.cpp : Defines the entry point for the application.
+//
+
+#include "BoxyProgrammingLanguage.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
