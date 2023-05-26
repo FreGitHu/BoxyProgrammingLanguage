ed5739d6a415ca9018572451d26e7873613a087e
Freddie <freddiejscott@outlook.com>
2023-05-26 18:57:12 +0100

    Add basic plugin class

--- BoxyProgrammingLanguage/plugins.h
+++ BoxyProgrammingLanguage/plugins.h
@@ -1,4 +1,24 @@
 #pragma once
+#include<thread>
+#include<iostream>
+class Plugin
+{
+public:
+	Plugin();
+	~Plugin();
+private:
+	std::thread pthread;
+};
+
+Plugin::Plugin(const std::string pluginname)
+{
+	std::cout << "plugin loaded " << pluginname;
+}
+
+Plugin::~Plugin()
+{
+}
+
 constexpr bool _iswindows() {
 	if (WIN32) {
 		return false;
